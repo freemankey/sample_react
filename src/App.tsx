a20db4db1f52cd3666ee9fa488824b6cc29c5679
freemankey <yta2489@gmail.com>
2021-08-08 17:32:22 +0900

    databaseからuser取得

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,15 @@ import { firebaseStore } from './firebase/index';
 
 function App() {
   const [users, setUsers] = useState<firebase.firestore.DocumentData[]>([]);
-  
-  const searchUsers = async() => {
+
+  const searchUsers = async () => {
     // Firestoreのコレクションを指定してデータ取得
     const res = await firebaseStore.collection('users').get();
     if (res.empty) return [];
     const userList: firebase.firestore.DocumentData[] = [];
     // DocumentData型にはmapメソッドが定義されていないため、forEachのループでデータを加工
     res.forEach(doc => {
-        userList.push(doc.data());
+      userList.push(doc.data());
     })
     setUsers(userList);
     console.log(users);
@@ -26,7 +26,7 @@ function App() {
   useEffect(() => {
 
   }, []);
-  
+
   searchUsers();
 
   return (
@@ -34,8 +34,8 @@ function App() {
       <div className="App-header">
         {
           users.map((user: any, index) => {
-          return <p key={index}> {user.name}</p>
-        })}
+            return <p key={index}> {user.name}</p>
+          })}
 
       </div>
     </div>
